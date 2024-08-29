ec68c34c9913f7336ccb70dcbbdb9fc34b0b5f13
rajivm05 <63921399+rajivm05@users.noreply.github.com>
2024-08-29 06:20:38 +0000

    initial commit;

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Rajiv Murali",
+  title: "Master of Computer Science, Player of Pianos",
+  email: "rajivmur@usc.edu",
+  gitHub: "",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
